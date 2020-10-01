6df35eff20a9c9ce015803a7dae882cc6ec9b56e
Dimitri Polonski <d.polonski@wmt.team>
2020-10-01 20:42:58 +0200

    document interactive.rs

--- src/interactive.rs
+++ src/interactive.rs
@@ -1,32 +1,38 @@
 use core::any::type_name;
 use core::fmt::Debug;
 
+/// The result type of most interactive methods
 pub type Result<'a, T> = core::result::Result<T, InteractiveError<'a>>;
 
+/// The main error type of this crate
 #[non_exhaustive]
 #[derive(Debug, PartialEq, Eq)]
 pub enum InteractiveError<'a> {
+    #[allow(missing_docs)]
     MethodNotFound {
         struct_name: &'a str,
         method_name: &'a str,
     },
+    #[allow(missing_docs)]
     FieldNotFound {
         struct_name: &'a str,
         field_name: &'a str,
     },
-    InstanceNotFound {
-        instance_name: &'a str,
-    },
-    WrongNumberOfArguments {
-        expected: usize,
-        found: usize,
-    },
+    #[allow(missing_docs)]
+    WrongNumberOfArguments { expected: usize, found: usize },
+    #[allow(missing_docs)]
+    ArgsError { given_args: &'a str },
+    #[allow(missing_docs)]
     SyntaxError,
-    ArgsError {
-        given_args: &'a str,
-    },
 }
 
+/// The main trait of this crate TODO
+///
+/// Note:
+/// It is currently not possible to check if a trait is implemented at runtime.
+/// This means that all members of an [`Interactive`] struct need to also implement [`Interactive`], which is why
+/// a default blanket implementation for all `T: Debug` is provided.
+///
 pub trait Interactive<'a, F, R>:
     Debug
     + InteractiveMethods<'a, F, R>
@@ -34,11 +40,13 @@ pub trait Interactive<'a, F, R>:
     + InteractiveFieldNames
     + InteractiveMethodNames
 {
+    /// Looks for a field with the given name and on success return a shared reference to it.
     fn __interactive_get_field(
         &'a self,
         field_name: &'a str,
     ) -> crate::Result<'a, &dyn crate::Interactive<'a, F, R>>;
 
+    /// Looks for a field with the given name and on success return a mutable reference to it.
     fn __interactive_get_field_mut(
         &'a mut self,
         field_name: &'a str,
@@ -70,7 +78,23 @@ where
     }
 }
 
+/// A trait that allows to interactively evaluate a field and pass its value to a given closure.
+///
+/// This trait gets implemented automatically when you derive [`Interactive`].
+///
+/// Note:
+/// It is currently not possible to check if a trait is implemented at runtime.
+/// This means that all members of an [`Interactive`] struct need to implement this trait, which is why
+/// a default blanket implementation for all `T: Debug` is provided.
+///
+/// [`Interactive`]: ./derive.Interactive.html
 pub trait InteractiveFields<'a, F, R>: Debug {
+    /// Looks for a field with the given name,
+    /// and passes it as a `Ok(&dyn Debug)` to the given closure.
+    ///
+    /// On error the an `Err(InteractiveError)` is passed to the closure instead.
+    ///
+    /// The return value of the closure is also returned by this method.
     fn __interactive_eval_field(&'a self, field_name: &'a str, f: F) -> R
     where
         F: Fn(Result<&dyn Debug>) -> R;
@@ -91,7 +115,26 @@ where
     }
 }
 
+/// A trait that allows to interactively evaluate a function and pass its result to a given closure.
+///
+/// This trait gets implemented automatically when you use the [`InteractiveMethods`] attribute.
+///
+/// Note:
+/// It is currently not possible to check if a trait is implemented at runtime.
+/// This means that all members of an [`Interactive`] struct need to implement this trait, which is why
+/// a default blanket implementation for all `T: Debug` is provided.
+///
+/// [`Interactive`]: ./derive.Interactive.html
+/// [`InteractiveMethods`]: ./attr.InteractiveMethods.html
 pub trait InteractiveMethods<'a, F, R>: Debug {
+    /// Looks for a method with the given name,
+    /// parses the args string into the expected arguments of the method,
+    /// executes the method and
+    /// passes the result as a `Ok(&dyn Debug)` to the given closure.
+    ///
+    /// On error the an `Err(InteractiveError)` is passed to the closure instead.
+    ///
+    /// The return value of the closure is also returned by this method.
     fn __interactive_eval_method(&'a mut self, method_name: &'a str, args: &'a str, f: F) -> R
     where
         F: Fn(Result<&dyn Debug>) -> R;
@@ -117,7 +160,18 @@ where
     }
 }
 
+/// A trait that allows a CLI to query all interactive field names.
+///
+/// This trait gets implemented automatically when you derive [`Interactive`].
+///
+/// Note:
+/// It is currently not possible to check if a trait is implemented at runtime.
+/// This means that all members of an [`Interactive`] struct need to implement this trait, which is why
+/// a default blanket implementation for all `T: Debug` is provided.
+///
+/// [`Interactive`]: ./derive.Interactive.html
 pub trait InteractiveFieldNames: Debug {
+    /// Returns all interactive field names of this type.
     fn get_all_interactive_field_names(&self) -> &'static [&'static str];
 }
 
@@ -130,7 +184,19 @@ where
     }
 }
 
+/// A trait that allows a CLI to query all interactive method names.
+///
+/// This trait gets implemented automatically when you use the [`InteractiveMethods`] attribute.
+///
+/// Note:
+/// It is currently not possible to check if a trait is implemented at runtime.
+/// This means that all members of an [`Interactive`] struct need to implement this trait, which is why
+/// a default blanket implementation for all `T: Debug` is provided.
+///
+/// [`Interactive`]: ./derive.Interactive.html
+/// [`InteractiveMethods`]: ./attr.InteractiveMethods.html
 pub trait InteractiveMethodNames: Debug {
+    /// Returns all interactive field names of this type.
     fn get_all_interactive_method_names(&self) -> &'static [&'static str];
 }
 
