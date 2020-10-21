ed0953734a92598d436fd2bc2a015d7307211bee
Dimitri Polonski <d.polonski@wmt.team>
2020-10-21 19:06:58 +0200

    lib doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,59 @@
-//! Docs and stuff TODO
-//! Default not neccessary
-//! # Known Limitations:
-//! * derive not implemented for Enums
-//! * interactive method args must implement parse
+//! This crate provides traits and macros that make your application's structs and functions interactive.
+//!
+//! Annotating a struct with `#[derive(Interactive)]`, a struct's methods with `#[Methods]`
+//! and a free function with `#[Function]` will implement a set of traits,
+//! that will allow you to access them as if Rust had a REPL.
+//!
+//! Use this crate as an alternative to "print debugging" or
+//! as an API replacement for a long running process.
+//!
+//! This crate is `no_std` compatible so you can use it to interact with embedded devices
+//! and blink those LEDs from a USB or UART connection.
+//!
+//! # Usage
+//! * Annotate everything you want to access with [`macro@Interactive`], [`macro@Methods`] and [`macro@Function`]
+//! * Define a new struct that owns or holds references to the objects you want to access
+//! * Derive [`macro@InteractiveRoot`] for it
+//! * Use the trait's methods to evaluate a string
+//! * The simplest one is [`eval_to_string`](InteractiveRoot::eval_to_string) but others allow for more custom behaviour
+//! * Accessing a field will give you its Debug representation
+//! * Calling a method will parse the arguments and give you the Debug representation of its return value
+//!
+//!
+//! Since this crate makes a lot of use of the [`Debug`] trait the helper macro [`PartialDebug`] is provided.
+//! It implements `Debug` for a struct replacing all fields that do not implement `Debug` with a placeholder.
+//!
+//! [`Debug`]: core::fmt::Debug
+//!
+//! ### CLI
+//! Functions like [`get_all_field_names`](Interactive::get_all_field_names) are provided.
+//! This makes it possible to implement things like auto-completion.
+//!
+//! Have a look at the autocomplete example for how this might be done using the [rustyline](https://docs.rs/crate/rustyline) crate.
 //!
 //! # Example
-//! ```no_run
+//! ```
+//! use minus_i::{Interactive, Methods, InteractiveRoot, Function, PartialDebug};
 //!
-//! use minus_i::{Interactive, Methods, InteractiveRoot};
+//! #[derive(Default)]
+//! struct NoDebug;
 //!
-//! #[derive(Interactive, Debug, Default)]
+//! #[derive(Interactive, PartialDebug, Default)]
 //! struct ChildStruct {
 //!     last_sum: f32,
+//!     no_debug: NoDebug,
 //! }
 //!
 //! #[Methods]
 //! impl ChildStruct {
-//!     fn add(&mut self, a: f32, b: f32) -> f32 {
+//!     fn add(&mut self, a: f32, b: f32) {
 //!         self.last_sum = a + b;
-//!         self.last_sum
 //!     }
 //! }
 //!
 //! #[derive(Interactive, Debug, Default)]
 //! struct ParentStruct {
-//!     child1: ChildStruct,
-//!     child2: ChildStruct,
+//!     child: ChildStruct,
 //! }
 //!
 //! #[derive(InteractiveRoot, Debug, Default)]
@@ -33,23 +61,52 @@
 //!     parent: ParentStruct,
 //! }
 //!
-//! fn main() -> std::io::Result<()> {
-//!     use std::io;
-//!     use std::io::Write;
+//! #[Function]
+//! fn add_one(a: u32) -> u32 {
+//!     a + 1
+//! }
 //!
-//!     let mut root = Root::default();
-//!     let mut input = String::new();
+//! let mut root = Root::default();
+//! assert_eq!(root.eval_to_string("parent.child.add(4.2, 6.9)"), "()");
+//! assert_eq!(root.eval_to_string("parent.child.last_sum"), "11.1");
+//! assert_eq!(root.eval_to_string("add_one(42)"), "43");
+//! assert_eq!(root.eval_to_string("parent.child"), "ChildStruct { last_sum: 11.1, no_debug: Unknown }");
+//! ```
 //!
-//!     loop {
-//!         print!(">>> ");
-//!         io::stdout().flush()?;
+//! # How it works
+//! This crate makes use of the unstable `specialization` feature, so it is only available on nightly.
 //!
-//!         input.clear();
-//!         io::stdin().read_line(&mut input)?;
-//!         println!("{}", root.eval_to_string(&input));
+//! Methods like `try_as_interactive` are implemented on all types.
+//! The method normally returns an error but in the specialized case
+//! a trait object (`&dyn Interactive` in this case) is returned.
+//!
+//! The macros then implement getters that look something like this:
+//! ```
+//! # use minus_i::*;
+//! # use minus_i::specialization::*;
+//! # struct Stub {
+//! #     field1: (),
+//! #     field2: (),
+//! # }
+//! # impl Stub {
+//! fn get_field<'a>(&'a self, field_name: &'a str) -> Result<'_, &dyn Interactive> {
+//!     match field_name {
+//!         "field1" => self.field1.try_as_interactive(),
+//!         "field2" => self.field2.try_as_interactive(),
+//!         _ => Err(InteractiveError::FieldNotFound {
+//!             type_name: "Struct",
+//!             field_name,
+//!         }),
 //!     }
 //! }
+//! # }
 //! ```
+//!
+//! See the macro's documentation for more details.
+//!
+//! # Current limitations:
+//! * Methods and functions can only be made interactive if their arguments implement [`FromStr`](core::str::FromStr)
+//! * Enums are not supported
 
 #![allow(incomplete_features)] // TODO re-enable warning
 #![feature(specialization)]
