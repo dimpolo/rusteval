4d8cbeacb582eee1909dd60fc7cc872749dde762
Dimitri Polonski <d.polonski@wmt.team>
2020-10-25 05:19:32 +0100

    all the escapes

--- src/arg_parse.rs
+++ src/arg_parse.rs
@@ -173,58 +173,103 @@ impl ArgParse for String {
     }
 }
 
+// "'A'" -> Ok('A')
 fn unescape_char(s: &str) -> Result<char, ArgParseError<'_>> {
-    match s.as_bytes() {
-        [b'\'', c, b'\''] => Ok(*c as char),
-        [b'\'', b'\\', c, b'\''] => match c {
-            b'n' => Ok('\n'),
-            b'r' => Ok('\r'),
-            b't' => Ok('\t'),
-            b'\\' => Ok('\\'),
-            b'\'' => Ok('\''),
-            b'"' => Ok('"'),
-
-            _ => Err(ArgParseError::UnescapeError(s)),
-        },
-        _ => Err(ArgParseError::UnescapeError(s)),
+    let mut chars = s.chars();
+    if chars.next() != Some('\'') {
+        return Err(ArgParseError::UnescapeError(s));
+    }
+    if chars.next_back() != Some('\'') {
+        return Err(ArgParseError::UnescapeError(s));
+    }
+    if chars.as_str().starts_with('\\') {
+        chars.next(); // pop '\\'
+        get_escaped_char(&mut chars).ok_or(ArgParseError::UnescapeError(s))
+    } else {
+        chars
+            .as_str()
+            .parse()
+            .map_err(ArgParseError::ParseCharError)
     }
 }
 
+// "\"asfd\"" -> Ok("asdf")
 fn unescape_str(s: &str) -> Result<String, ArgParseError<'_>> {
-    if s.len() >= 2 && s.starts_with('"') && s.ends_with('"') {
-        let mut chars = s[1..s.len() - 1].chars();
+    let mut chars = s.chars();
+    if chars.next() != Some('\"') {
+        return Err(ArgParseError::UnescapeError(s));
+    }
+    if chars.next_back() != Some('\"') {
+        return Err(ArgParseError::UnescapeError(s));
+    }
 
-        let mut res = String::with_capacity(s.len());
+    let mut res = String::with_capacity(chars.as_str().len() - 2);
 
-        while let Some(c) = chars.next() {
-            if c == '\\' {
-                match chars.next() {
-                    None => {
-                        return Err(ArgParseError::UnescapeError(s));
-                    }
-                    Some(c2) => {
-                        res.push(match c2 {
-                            'n' => '\n',
-                            'r' => '\r',
-                            't' => '\t',
-                            '\\' => '\\',
-                            '\'' => '\'',
-                            '"' => '"',
-                            _ => {
-                                return Err(ArgParseError::UnescapeError(s));
-                            }
-                        });
-                        continue;
-                    }
-                };
+    while let Some(c) = chars.next() {
+        match c {
+            '\\' => {
+                let c = get_escaped_char(&mut chars).ok_or(ArgParseError::UnescapeError(s))?;
+                res.push(c)
             }
-
-            res.push(c);
+            _ => res.push(c),
         }
+    }
 
-        Ok(res)
+    Ok(res)
+}
+
+// "n" -> Some('\n')
+fn get_escaped_char(after_backslash: &mut core::str::Chars<'_>) -> Option<char> {
+    match after_backslash.next() {
+        None => None,
+        Some(char) => match char {
+            'n' => Some('\n'),
+            'r' => Some('\r'),
+            't' => Some('\t'),
+            '\\' => Some('\\'),
+            '0' => Some('\0'),
+            '\'' => Some('\''),
+            '\"' => Some('\"'),
+            'x' => parse_hex(after_backslash),
+            'u' => parse_unicode(after_backslash),
+            _ => None,
+        },
+    }
+}
+
+// "41" -> Some('A')
+fn parse_hex(after_x: &mut core::str::Chars<'_>) -> Option<char> {
+    let hex = after_x.as_str().get(..2)?;
+    let u = u32::from_str_radix(hex, 16).ok()?;
+    if u > 0x7f {
+        return None;
+    };
+    let res = core::char::from_u32(u);
+
+    // pop used chars
+    after_x.next();
+    after_x.next();
+    res
+}
+
+// "{2764}" -> Some('❤')
+fn parse_unicode(after_u: &mut core::str::Chars<'_>) -> Option<char> {
+    if after_u.next() != Some('{') {
+        return None;
+    }
+    if let Some(hex_end) = after_u.as_str().find('}') {
+        let hex = &after_u.as_str()[..hex_end];
+        let res = u32::from_str_radix(hex, 16)
+            .ok()
+            .and_then(core::char::from_u32);
+
+        // pop used chars
+        for _ in 0..=hex_end {
+            after_u.next();
+        }
+        res
     } else {
-        Err(ArgParseError::UnescapeError(s))
+        None
     }
 }
 
@@ -269,13 +314,26 @@ mod tests {
     }
 
     #[test]
+    fn test_heart() {
+        test_parse_one_arg("'\\u{2764}'", '❤');
+    }
+
+    #[test]
+    fn test_hex() {
+        test_parse_one_arg("'\\x41'", 'A');
+    }
+
+    #[test]
     fn test_easy_string() {
         test_parse_one_arg("\"test\"", String::from("test"));
     }
 
     #[test]
-    fn test_string() {
-        test_parse_one_arg("\"test\\ntest\"", String::from("test\ntest"));
+    fn test_complex_string() {
+        test_parse_one_arg(
+            "\" test \\n '\\u{2764}' \\r \\\"täst\\\" \\x41\"",
+            String::from(" test \n '❤' \r \"täst\" A"),
+        );
     }
 
     #[test]
